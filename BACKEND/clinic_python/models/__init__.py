5e2162b3fe66c72cd8da63904895e0adda9adc5c
Ejay <domen.ejaymatthew@gmail.com>
2024-12-23 09:17:03 +0800

    updated backend

--- BACKEND/clinic_python/models/__init__.py
+++ BACKEND/clinic_python/models/__init__.py
@@ -1,2 +1,5 @@
 from .patient_model import Patient
 from .roles_model import Role
+from .admin_model import Staff
+from .superadmin_model import SuperAdmin
+
--- BACKEND/clinic_python/models/queuemanagement_model.py
+++ BACKEND/clinic_python/models/queuemanagement_model.py
@@ -14,8 +14,6 @@ class QueueManagement(models.Model):
     transaction_type = models.CharField(
         max_length=20, choices=TRANSACTION_TYPE_CHOICES, default="Consultation"
     )
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
 
     def __str__(self):
         return f'Queue {self.status} for {self.date}'
