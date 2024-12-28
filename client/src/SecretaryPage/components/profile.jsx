d0ee1ab7f4ab6516185940d5fc7745874891ab95
Ejay <domen.ejaymatthew@gmail.com>
2024-12-29 00:49:28 +0800

    profile

--- client/src/SecretaryPage/components/profile.jsx
+++ client/src/SecretaryPage/components/profile.jsx
@@ -18,7 +18,7 @@ const Profile = () => {
   const [isEditable, setIsEditable] = useState(false); // Toggle for edit mode
 
   const fetchProfile = async () => {
-    try {
+    try { 
       const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/admin/me/`, {
         headers: {
           'Authorization': `Bearer ${localStorage.getItem('token')}`  // Assuming token-based authentication
