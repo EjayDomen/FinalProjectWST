fb58dd4e9780f86317313feec94764bee4314f44
Ejay <domen.ejaymatthew@gmail.com>
2025-02-02 00:20:36 +0800

    updated

--- BACKEND/clinic_python/settings.py
+++ BACKEND/clinic_python/settings.py
@@ -125,16 +125,19 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+
 
 # Default auto field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWS_CREDENTIALS =  True
+
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
